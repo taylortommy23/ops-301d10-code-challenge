ef0089952c8c94042eaf1b6816e94e5b65fcecd0
taylortommy23 <147282624+taylortommy23@users.noreply.github.com>
2023-12-05 01:42:25 -0600

    Create op_challenge06.py

---
+++ op_challenge06.py
@@ -0,0 +1,20 @@
+#!/bin/python
+# Script Name:         op_challenge06.py
+# Author:              Tommy Taylor
+# Date:                12/4/23
+
+
+# import OS library
+import os
+
+# Variables 
+
+user = 'whoami'
+ipaddress = 'ip a'
+hardware = 'lshw -short' 
+
+# Output
+
+print(user)
+print(ipaddress)
+print(hardware)
