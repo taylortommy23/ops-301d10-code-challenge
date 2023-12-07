99888434af3b575e27e0e8bb5357ef9bcdbf5f91
taylortommy23 <147282624+taylortommy23@users.noreply.github.com>
2023-12-06 18:52:58 -0600

    Create ops_challenge08.py

---
+++ ops_challenge08.py
@@ -0,0 +1,19 @@
+# Assign a list of ten string elements to a variable
+my_list = ["apple", "banana", "cherry", "date", "fig", "grape", "honeydew", "kiwi", "lemon", "mango"]
+
+# Print the fourth element of the list
+print("Fourth element:", my_list[3])
+
+# Print the sixth through tenth element of the list
+print("Sixth through tenth elements:", my_list[5:])
+
+# Change the value of the seventh element to “onion”
+my_list[6] = "onion"
+
+# Print the modified list
+print("Modified list:", my_list)
+
+
+
+
+
