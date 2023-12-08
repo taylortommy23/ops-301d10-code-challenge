97893516c8b7e0f05143323273d8f3f086dec5b1
taylortommy23 <147282624+taylortommy23@users.noreply.github.com>
2023-12-08 00:12:07 -0600

    Create ops_challenge09.py

---
+++ ops_challenge09.py
@@ -0,0 +1,19 @@
+# Sample values for a and b
+a = 5
+b = 10
+
+# Example 1: if statement with logical conditionals and elif
+if a == b:
+    print("a is equal to b")
+elif a < b:
+    print("a is less than b")
+elif a > b:
+    print("a is greater than b")
+else:
+    print("None of the conditions are met")
+
+# Example 2: if statement with logical conditionals and else
+if a != b:
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
